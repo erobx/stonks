1a266b66bbba9e9923db4afd7026f16b0e22f7e4
Evan Robinson <erob7856@gmail.com>
2022-11-26 15:39:33 -0500

    updated db

--- stonks/__init__.py
+++ stonks/__init__.py
@@ -4,14 +4,27 @@ RUN THIS FILE TO HOST ON LOCAL HOST WITH DEBUGGER
 
 """
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "stonks.sqlite")
     )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     # routes to index page
     @app.route("/")
@@ -23,8 +36,8 @@ def create_app(test_config=None):
     def visualizer():
         return render_template("visualizer.html")
 
-    #from stonks import db
-    #db.init_app(app)
+    from stonks import db
+    db.init_app(app)
 
     from stonks import net
     net.init_network()
--- stonks/schema.sql
+++ stonks/schema.sql
@@ -1,2 +1,11 @@
 --This is where we will define how our data is stored
 --TICKER    P/E     VOLUME      PRICE       MARKET CAP      MORE?
+
+DROP TABLE IF EXISTS stock;
+
+CREATE TABLE stock (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ticker TEXT UNIQUE NOT NULL,
+    price FLOAT NOT NULL,
+    p_e FLOAT NOT NULL
+);
