03a7ade10b94a8bea29dc9259d096f752136c688
Donny Arango <darango3002@gmail.com>
2022-12-06 16:10:36 -0500

    finding stock index

--- query.py
+++ query.py
@@ -2,17 +2,24 @@ import sqlite3
 
 # logo, ticker, pe, volume, price, market_cap, eps, sector, employees, revenue, growth, profit
 
-def query(db_file):
+def query_first5_at_ticker(db_file, id):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
-        select_all = "SELECT logo, ticker, volume, price, sector FROM stock"
-        rows = cursor.execute(select_all).fetchall()
-        for r in rows:
+        find_ticker = "SELECT rowid, ticker, volume FROM stock ORDER BY volume DESC"
+        cursor.execute(find_ticker)  
+        data = cursor.fetchall()
+
+        index = None
+        for i,r in enumerate(data):
+            if (data[i][1] == id):
+                index = i
             print(r)
+
+        print("ticker at index:", index)
+
     finally:
         if conn:
             conn.close()
-
-query('stonks.db')
+query_first5_at_ticker('stonks.db', 'CVE')
