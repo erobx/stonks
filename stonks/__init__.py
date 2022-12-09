923ad72358ccd17ffaa43e04dabe89d0c540e1aa
Evan Robinson <erob7856@gmail.com>
2022-12-09 16:40:32 -0500

    routing

--- stonks/__init__.py
+++ stonks/__init__.py
@@ -4,7 +4,7 @@ RUN THIS FILE TO HOST ON LOCAL HOST WITH DEBUGGER
 
 """
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pyvis.network import Network
 
 def create_app(test_config=None):
@@ -26,14 +26,17 @@ def create_app(test_config=None):
         return render_template("index.html")
 
     db_path = os.path.abspath('stonks/stonks.db')
-    network = Network(height="100vh", neighborhood_highlight=True)
-    network.toggle_physics(True)
     from . import net
-    src = net.init_network(db_path, net=network, id='AAPL', sort='price', depth=10, k=10)
 
     # route to visualizer page
-    @app.route("/visualizer")
+    @app.route("/visualizer", methods=["POST"])
     def visualizer():
+        if request.method == "POST":
+            ticker = request.form["ticker"]
+            network = Network(height="100vh", neighborhood_highlight=True)
+            network.toggle_physics(True)
+            src = net.init_network(db_path, net=network, id=ticker, sort='price', depth=10, k=10)
+
         return render_template("visualizer.html")
 
 
